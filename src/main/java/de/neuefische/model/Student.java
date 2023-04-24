60a0e0111424696cc3ac4d8a4285054536dff9d9
mjaschew <dermijasch@gmx.de>
2023-04-24 15:46:50 +0200

    StudentDB class created

--- src/main/java/de/neuefische/model/Student.java
+++ src/main/java/de/neuefische/model/Student.java
@@ -7,7 +7,7 @@ public class Student {
         private String name;
         private int id;
     //Methods
-        
+
         @Override
         public String toString() {
             return "Student{" +
--- src/main/java/de/neuefische/model/StudentDB.java
+++ src/main/java/de/neuefische/model/StudentDB.java
@@ -3,5 +3,22 @@ package de.neuefische.model;
 import java.util.Objects;
 
 public class StudentDB {
+    //Properties
+        private Student[] database;
+    //Constructor
+        public StudentDB(Student[] database) {
+            this.database = database;
+        }
+        public StudentDB() {
+        }
 
+    //Getter & Setter
+
+        public Student[] getDatabase() {
+            return database;
+        }
+
+        public void setDatabase(Student[] database) {
+            this.database = database;
+        }
 }
